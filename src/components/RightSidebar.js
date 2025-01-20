69b3818a5ebab13ce0b61501f3caa6327605ff8b
Roy Yali <ryali93@gmail.com>
2025-01-20 19:31:15 +0100

    rightsidebar template works

--- src/components/RightSidebar.js
+++ src/components/RightSidebar.js
@@ -1,46 +1,17 @@
-// src/components/RightSidebar.js
 import React, { useState } from 'react';
 import '../styles/RightSidebar.css';
-import { FaTools, FaSlidersH, FaCalendarAlt } from 'react-icons/fa';
+import GeeTools from '../tools/GeeTools';
+import VhrTools from '../tools/VhrTools';
+import InsarTools from '../tools/InsarTools';
 
 function RightSidebar({ isCollapsed, onToggle }) {
   const [activeTab, setActiveTab] = useState('Tool1'); // Tab activa por defecto
 
   // Herramientas disponibles
   const tools = {
-    Tool1: {
-      label: 'Tool 1',
-      content: (
-        <div>
-          <h4>Tool 1</h4>
-          <input type="text" placeholder="Enter value" />
-          <button>Run Tool 1</button>
-        </div>
-      ),
-      icon: <FaTools />,
-    },
-    Tool2: {
-      label: 'Tool 2',
-      content: (
-        <div>
-          <h4>Tool 2</h4>
-          <input type="range" min="0" max="100" />
-          <button>Run Tool 2</button>
-        </div>
-      ),
-      icon: <FaSlidersH />,
-    },
-    Tool3: {
-      label: 'Tool 3',
-      content: (
-        <div>
-          <h4>Tool 3</h4>
-          <input type="date" />
-          <button>Run Tool 3</button>
-        </div>
-      ),
-      icon: <FaCalendarAlt />,
-    },
+    Tool1: GeeTools,
+    Tool2: VhrTools,
+    Tool3: InsarTools,
   };
 
   const activateTab = (tab) => {
@@ -52,13 +23,11 @@ function RightSidebar({ isCollapsed, onToggle }) {
 
   return (
     <div className={`right-sidebar ${isCollapsed ? 'collapsed' : ''}`}>
-      {/* Botón de colapsar/expandir */}
       <div className="toggle-wrapper">
         <button className="toggle-button" onClick={onToggle}>
           {isCollapsed ? '>' : '<'}
         </button>
       </div>
-      {/* Contenido */}
       {!isCollapsed ? (
         <div className="sidebar-content">
           <div className="tabs">
@@ -72,7 +41,9 @@ function RightSidebar({ isCollapsed, onToggle }) {
               </button>
             ))}
           </div>
-          <div className="tool-content">{tools[activeTab].content}</div>
+          <div className="tool-content">
+            {React.createElement(tools[activeTab].content)} {/* Renderiza el componente */}
+          </div>
         </div>
       ) : (
         <div className="collapsed-tools">
